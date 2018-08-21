9584f073c8db4b85cc7eee3299e3b893e9de5ee3
ivmartel <ivmartel@gmail.com>
2018-08-22 00:00:20 +0200

    Fix tests bis.

--- src/app/dwv/dwv.component.spec.ts
+++ src/app/dwv/dwv.component.spec.ts
@@ -33,13 +33,15 @@ describe('DwvComponent', () => {
   it(`should have a legend`, async(() => {
     const fixture = TestBed.createComponent(DwvComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.legend).toContain('Powered by <a href="https://github.com/ivmartel/dwv" title="dwv on github">dwv</a>');
+    expect(app.legend).toContain(
+      'Powered by <a href="https://github.com/ivmartel/dwv" title="dwv on github">dwv</a>');
   }));
 
   it('should render legend with legend class', async(() => {
     const fixture = TestBed.createComponent(DwvComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.legend').textContent).toContain('Powered by <a href="https://github.com/ivmartel/dwv" title="dwv on github">dwv</a>');
+    expect(compiled.querySelector('.legend').textContent).toContain(
+      'Powered by <a href="https://github.com/ivmartel/dwv" title="dwv on github">dwv</a>');
   }));
 });
