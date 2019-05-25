a6e7f12ffc049e0f6f5598333f45a48403772dc8
ivmartel <ivmartel@gmail.com>
2019-05-25 23:29:38 +0200

    Activate service worker.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { DwvModule } from './dwv/dwv.module';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [ AppComponent ],
-  imports: [ DwvModule ],
+  imports: [ DwvModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }) ],
   bootstrap: [AppComponent]
 })
 
