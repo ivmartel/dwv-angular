ba2bcfd98e33817d0f7a3d23191f59a0681fe95a
ivmartel <ivmartel@gmail.com>
2019-08-05 22:51:19 +0200

    Add components tests.

---
+++ src/app/dwv/tags-dialog.component.spec.ts
@@ -0,0 +1,49 @@
+import { Component, Input } from '@angular/core';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+import { TagsDialogComponent } from './tags-dialog.component';
+
+describe('TagsDialogComponent', () => {
+  let component: TagsDialogComponent;
+  let fixture: ComponentFixture<TagsDialogComponent>;
+
+  @Component({
+      selector: 'app-dwv-tags-table',
+      template: ''
+  })
+  class FakeTagsTableComponent {
+      @Input() data: any;
+  }
+
+  beforeEach(async(() => {
+      TestBed.configureTestingModule({
+          imports: [
+              MatDialogModule
+          ],
+          declarations: [
+              TagsDialogComponent,
+              FakeTagsTableComponent
+          ],
+          providers: [
+              { provide: MAT_DIALOG_DATA, useValue: {} },
+              { provide: MatDialogRef, useValue: {} }
+          ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+      fixture = TestBed.createComponent(TagsDialogComponent);
+      component = fixture.debugElement.componentInstance;
+  });
+
+  it('should create', () => {
+      expect(component).toBeTruthy();
+  });
+
+  it('should init', () => {
+      expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+});
---
+++ src/app/dwv/tags-table.component.spec.ts
@@ -0,0 +1,52 @@
+import { Component, Input } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+
+import { TagsTableComponent } from './tags-table.component';
+
+describe('TagsTableComponent', () => {
+  let component: TagsTableComponent;
+  let fixture: ComponentFixture<TagsTableComponent>;
+
+  @Component({
+      selector: 'mat-paginator',
+      template: ''
+  })
+  class FakePaginatorComponent {
+      @Input() pageSizeOptions: any;
+  }
+
+  beforeEach(async(() => {
+      TestBed.configureTestingModule({
+          imports: [
+              BrowserAnimationsModule,
+              FormsModule,
+              MatInputModule,
+              MatFormFieldModule,
+              MatTableModule
+          ],
+          declarations: [
+              TagsTableComponent,
+              FakePaginatorComponent
+          ]
+      }).compileComponents();
+  }));
+
+  beforeEach(() => {
+      fixture = TestBed.createComponent(TagsTableComponent);
+      component = fixture.debugElement.componentInstance;
+  });
+
+  it('should create', () => {
+      expect(component).toBeTruthy();
+  });
+
+  it('should init', () => {
+      expect(() => fixture.detectChanges()).not.toThrow();
+  });
+
+});
