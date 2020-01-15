ae1469c96457da87e4a95201c1218f27eebdd86f
ivmartel <ivmartel@gmail.com>
2020-01-15 23:33:47 +0100

    First go at update: recreate same behaviour.

--- src/app/dwv/dwv.component.ts
+++ src/app/dwv/dwv.component.ts
@@ -6,14 +6,8 @@ import { TagsDialogComponent } from './tags-dialog.component';
 
 // gui overrides
 
-// decode query
-dwv.utils.decodeQuery = dwv.utils.base.decodeQuery;
-// progress
-dwv.gui.displayProgress = () => {};
 // get element
 dwv.gui.getElement = dwv.gui.base.getElement;
-// refresh element
-dwv.gui.refreshElement = dwv.gui.base.refreshElement;
 
 // Image decoders (for web workers)
 dwv.image.decoderScripts = {
@@ -32,11 +26,17 @@ dwv.image.decoderScripts = {
 export class DwvComponent implements OnInit {
   public versions: any;
   public tools = ['Scroll', 'ZoomAndPan', 'WindowLevel', 'Draw'];
+  public shapes = ['Ruler'];
   public selectedTool = 'Select Tool';
   public loadProgress = 0;
   public dataLoaded = false;
   private dwvApp: any;
-  private tags: any[];
+  private metaData: any[];
+
+  // drop box class name
+  private dropboxClassName = "dropBox";
+  private borderClassName = "dropBoxBorder";
+  private hoverClassName = "hover";
 
   constructor(public dialog: MatDialog) {
     this.versions = {
@@ -52,48 +52,170 @@ export class DwvComponent implements OnInit {
     this.dwvApp.init({
       containerDivId: 'dwv',
       tools: this.tools,
-      shapes: ['Ruler'],
-      isMobile: true
+      shapes: this.shapes
     });
     // progress
-    const self = this;
     this.dwvApp.addEventListener('load-progress', (event) => {
-      self.loadProgress = event.loaded;
+      this.loadProgress = event.loaded;
     });
     this.dwvApp.addEventListener('load-end', (event) => {
       // set data loaded flag
-      self.dataLoaded = true;
+      this.dataLoaded = true;
       // set dicom tags
-      self.tags = self.dwvApp.getTags();
+      this.metaData = this.objectToArray(this.dwvApp.getMetaData());
       // set the selected tool
-      if (self.dwvApp.isMonoSliceData() && self.dwvApp.getImage().getNumberOfFrames() === 1) {
-        self.selectedTool = 'ZoomAndPan';
+      if (this.dwvApp.isMonoSliceData() && this.dwvApp.getImage().getNumberOfFrames() === 1) {
+        this.selectedTool = 'ZoomAndPan';
       } else {
-        self.selectedTool = 'Scroll';
+        this.selectedTool = 'Scroll';
       }
+      this.onChangeTool(this.selectedTool);
     });
+    // setup drop box
+    this.setupDropbox();
   }
 
-  onChangeTool(tool): void {
+  /**
+   * Setup the drop box: add event listeners and set initial size.
+   */
+  setupDropbox = () => {
+      // start listening to drag events on the layer container
+      var layerContainer = this.dwvApp.getElement("layerContainer");
+      if (layerContainer) {
+        layerContainer.addEventListener("dragover", this.onDragOver);
+        layerContainer.addEventListener("dragleave", this.onDragLeave);
+        layerContainer.addEventListener("drop", this.onDrop);
+      }
+      // set the initial drop box size
+      var box = this.dwvApp.getElement(this.dropboxClassName);
+      if (box) {
+        var size = this.dwvApp.getLayerContainerSize();
+        var dropBoxSize = 2 * size.height / 3;
+        box.setAttribute("style","width:"+dropBoxSize+"px;height:"+dropBoxSize+"px");
+      }
+  }
+
+  hideDropbox = () => {
+    // remove box
+    var box = this.dwvApp.getElement(this.dropboxClassName);
+    if (box) {
+      box.parentNode.removeChild(box);
+    }
+  }
+
+  /**
+   * Handle a change tool event.
+   * @param {String} tool The new tool.
+   */
+  onChangeTool = (tool) => {
     if ( this.dwvApp ) {
       this.selectedTool = tool;
-      this.dwvApp.onChangeTool({ currentTarget: { value: tool } });
+      this.dwvApp.setTool(tool);
+      if (tool === 'Draw') {
+        this.onChangeShape(this.shapes[0]);
+      }
     }
   }
 
-  onReset(): void {
+  /**
+   * Handle a change draw shape event.
+   * @param {String} shape The new shape.
+   */
+  onChangeShape = (shape) => {
+    if ( this.dwvApp && this.selectedTool === 'Draw') {
+      this.dwvApp.setDrawShape(shape);
+    }
+  }
+
+  /**
+   * Handle a reset event.
+   */
+  onReset = () => {
     if ( this.dwvApp ) {
-      this.dwvApp.onDisplayReset();
+      this.dwvApp.resetDisplay();
     }
   }
 
-  openTagsDialog(): void {
+  /**
+   * Open the DICOM tags dialog.
+   */
+  openTagsDialog = () => {
     this.dialog.open(TagsDialogComponent,
       {
         width: '80%',
         height: '90%',
-        data: { title: 'DICOM Tags', value: this.tags }
+        data: { title: 'DICOM Tags', value: this.metaData }
       }
     );
   }
+
+  /**
+   * Handle a drag over.
+   * @param {Object} event The event to handle.
+  */
+  onDragOver = (event) => {
+    // prevent default handling
+    event.stopPropagation();
+    event.preventDefault();
+    // update box border
+    var box = this.dwvApp.getElement(this.borderClassName);
+    if (box && box.className.indexOf(this.hoverClassName) === -1) {
+        box.className += " " + this.hoverClassName;
+    }
+  }
+
+  /**
+    * Handle a drag leave.
+    * @param {Object} event The event to handle.
+   */
+  onDragLeave = (event) => {
+    // prevent default handling
+    event.stopPropagation();
+    event.preventDefault();
+    // update box class
+    var box = this.dwvApp.getElement(this.borderClassName + " hover");
+    if (box && box.className.indexOf(this.hoverClassName) !== -1) {
+        box.className = box.className.replace(" " + this.hoverClassName, "");
+    }
+  }
+
+  /**
+   * Handle a drop event.
+   * @param {Object} event The event to handle.
+   */
+  onDrop = (event) => {
+    // prevent default handling
+    event.stopPropagation();
+    event.preventDefault();
+    // load files
+    this.dwvApp.loadFiles(event.dataTransfer.files);
+    // hide drop box
+    this.hideDropbox();
+  }
+
+  isObject = (unknown) => {
+      var type = typeof unknown;
+      return type === 'function' || type === 'object' && !!unknown;
+  }
+
+  objectToArray = (obj) => {
+      var array = [];
+      var keys = Object.keys(obj);
+      for (var i = 0; i < keys.length; ++i ) {
+          var key = keys[i];
+          var row = {name: key};
+          var innerKeys = Object.keys(obj[key]);
+          for (var j = 0; j < innerKeys.length; ++j ) {
+              var innerKey = innerKeys[j];
+              var value = obj[key][innerKey];
+              if (this.isObject(value)) {
+                  value = JSON.stringify(value);
+              }
+              row[innerKey] = value;
+          }
+          array.push(row);
+      }
+      return array;
+  }
+
 }
