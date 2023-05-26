62985db1521cb5f2c57c9f41d4b48f57e0af9f09
ivmartel <ivmartel@gmail.com>
2023-05-26 15:00:33 +0200

    Update tabs

--- src/app/dwv/tags-dialog.component.ts
+++ src/app/dwv/tags-dialog.component.ts
@@ -2,14 +2,14 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-    selector: 'app-dwv-tags-dialog',
-    templateUrl: './tags-dialog.component.html'
+  selector: 'app-dwv-tags-dialog',
+  templateUrl: './tags-dialog.component.html'
 })
 
 export class TagsDialogComponent {
 
-    constructor(
-        public dialogRef: MatDialogRef<TagsDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(
+    public dialogRef: MatDialogRef<TagsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {}
 
 }
